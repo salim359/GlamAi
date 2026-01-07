44746adb5a2c40c192c1c8d678ebe8c69fb5bcf6
Noufa Salim <snoufa3@gmail.com>
2026-01-07 17:07:19 +0530

    Add DynamoDBStack to main application

--- Backend/lib/index.ts
+++ Backend/lib/index.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import { CognitoStack } from "./cognito";
 import S3Stack from "./s3";
+import { DynamoDBStack } from "./db";
 
 interface AppConfig {
     app: cdk.App;
@@ -8,6 +9,7 @@ interface AppConfig {
 
 export default function main(app: cdk.App): void {
     new CognitoStack(app, "cognito");
-
     new S3Stack(app, "s3");
+    new DynamoDBStack(app, "dynamodb");
+
 }
